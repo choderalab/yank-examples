076e0dc84196745e8e385d5f2017ee5bb588b249
Levi Naden <levi.naden@choderalab.org>
2016-11-07 14:59:46 -0500

    Added the all ligand binding example

---
+++ examples/binding/all-ligands/run-explicit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# multiple ligands binding to T4 lysozyme L99A example run script (serial mode)
+#
+
+# Run the simulation
+echo "Running simulation..."
+yank script --yaml=explicit.yaml
+
+# Analyze the data
+echo "Analyzing data..."
+yank analyze --store=experiments
