fc81718ec9e354d6623ef64ca52876a5f04d889c
John Chodera <choderaj@mskcc.org>
2016-11-28 14:27:08 -0500

    Updates to Abl:imatinib

--- examples/binding/abl-imatinib/run-torque-explicit.sh
+++ examples/binding/abl-imatinib/run-torque-explicit.sh
@@ -3,7 +3,7 @@
 #
 #
 # walltime : maximum wall clock time (hh:mm:ss)
-#PBS -l walltime=24:00:00
+#PBS -l walltime=72:00:00
 #
 # join stdout and stderr
 #PBS -j oe
@@ -16,7 +16,7 @@
 #
 # nodes: number of nodes
 #   ppn: how many cores per node to use
-#PBS -l nodes=1:ppn=4:gpus=4:shared
+#PBS -l nodes=4:ppn=4:gpus=4:shared:gtx680
 #
 # export all my environment variables to the job
 ##PBS -V
@@ -27,6 +27,10 @@
 if [ -n "$PBS_O_WORKDIR" ]; then 
     cd $PBS_O_WORKDIR
 fi
+
+cat $PBS_GPUFILE
+
+nvcc --version
 
 # Run the simulation with verbose output:
 echo "Running simulation via MPI..."
