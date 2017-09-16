25e57a902110bed2cac12a0cffc37a9311b78e63
Levi Naden <levi.naden@choderalab.org>
2017-09-16 16:11:48 -0400

    Updated the freesolv set for yank17

---
+++ examples/hydration/freesolv/run-lsf-yank.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#  Batch script for mpirun job on cbio LSF cluster
+#  Adjust your script as needed for your clusters!
+#
+# walltime : maximum wall clock time (hh:mm)
+#BSUB -W 6:00
+#
+# Set Output file
+#BSUB -o  freesolv-mini.%J.log
+#
+# Specify node group
+#BSUB -m ls-gpu
+#
+# nodes: number of nodes and GPU request
+# 8 CPU and GPU over 2 nodes
+#BSUB -n 4 -R "rusage[ngpus_excl_p=1,mem=8]"
+#
+# Start MPS since Cbio GPUs are in exclusive mode
+#BSUB -env "all, LSB_START_JOB_MPS=Y"
+#
+# job name (default = name of script file)
+#BSUB -J "freesolv-mini"
+
+# Run the simulation with verbose output:
+echo "Running simulation via MPI..."
+build_mpirun_configfile "yank script --yaml=yank.yaml"
+mpiexec.hydra -f hostfile -configfile configfile
+date
+
