8b6283a06144d2ab28ff1b0c8101c331006d73ba
John Chodera <john.chodera@choderalab.org>
2017-09-17 19:33:48 +0200

    More tinkering.

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -16,7 +16,9 @@ bash $MINICONDA -b -p $MINICONDA_HOME
 # Configure miniconda
 export PIP_ARGS="-U"
 export PATH=$MINICONDA_HOME/bin:$PATH
-conda config --add channels conda-forge  
+conda config --add channels omnia/label/dev
+conda config --add channels omnia
+conda config --add channels conda-forge
 conda update -yq conda
 conda install --yes conda-build jinja2 anaconda-client pip
 
