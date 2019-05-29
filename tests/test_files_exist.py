e1087d3c592f331b202bf26106b4f706c606283a
Jaime RGP <jaime.rogue@gmail.com>
2019-05-29 12:06:11 +0200

    test that files are indeed copied over to sys.prefix and that they match the ones in the repo

---
+++ tests/test_files_exist.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+# =============================================================================================
+# MODULE DOCSTRING
+# =============================================================================================
+
+"""
+Test YAML syntax in examples.
+
+"""
+
+# =============================================================================================
+# GLOBAL IMPORTS
+# =============================================================================================
+
+from pathlib import Path
+import filecmp
+import os
+import sys
+
+import pytest
+import yaml
+
+
+def _dirs_equal(comparer):
+    """
+    Recurse over subdirs testing that there are no:
+    - files with same name, but different contents
+    - files with different names
+    """
+    yield all(not bool(x) for x in (comparer.diff_files, comparer.left_only, comparer.right_only))
+    for name, subdir in comparer.subdirs.items():
+        print(name)
+        yield from _dirs_equal(subdir)
+
+
+def test_files_exist():
+    """
+    Compare examples copied to $PREFIX/share/yank
+    and those available in the current repo. They should match!
+    """
+    repo_examples = Path(__file__).parent.parent / 'examples'
+    prefix_examples = Path(sys.prefix) / 'share' / 'yank' / 'examples'
+    assert repo_examples.is_dir()
+    assert prefix_examples.is_dir()
+    comparer = filecmp.dircmp(repo_examples, prefix_examples)
+    assert all(_dirs_equal(comparer))
